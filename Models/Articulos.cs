248569713ee1c6a7dfb3a06dbb326385bc0c62f9
Victor Jose Guichardo Concepcion <victor_guichardo@ucne.edu.do>
2020-10-07 18:44:47 -0400

    Model creado

---
+++ Models/Articulos.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VictorP1_AP2.Models{
+
+    public class Articulos{
+
+        [Key]
+        public int ArticuloId{get; set;}
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public string Descripcion{get; set;}
+        [Required,Range(minimum:1,maximum:int.MaxValue,ErrorMessage = "La existencia debe ser mayor de cero")]
+        public int Existencia{get; set;}
+        [Required,Range(minimum:1,maximum:int.MaxValue,ErrorMessage = "El costo debe ser mayor que cero")]
+        public float Costo{get; set;}
+        [Required,Range(minimum:1,maximum:int.MaxValue,ErrorMessage = "El valor del inventario debe ser mayor que cero")]
+        public float ValorInventario{get; set;}
+
+    }
+    public class ArticulosDetalle{
+
+        [Key]
+        public int Id{get; set;}
+        public int ArticuloId{get; set;}
+
+    }
+    
+}
